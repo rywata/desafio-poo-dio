40c14e61c69ac7de521f5e929956a5d2426e0dd1
rafaelywata <rafaelywata@gmail.com>
2024-06-05 15:20:15 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -24,8 +22,41 @@ public class Main {
         mentoria.setDescricao("Descricao: Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao: Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTenshi = new Dev();
+        devTenshi.setNome("Tenshi");
+        devTenshi.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Tenshi: " + devTenshi.getConteudosInscritos());
+        devTenshi.progredir();
+        devTenshi.progredir();
+        devTenshi.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Tenshi: " + devTenshi.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Tenshi: " + devTenshi.getConteudosConcluidos());
+        System.out.println("XP: " + devTenshi.calcularTotalXp());
+
+        System.out.println("--------");
+
+        Dev devAiko = new Dev();
+        devAiko.setNome("Aiko");
+        devAiko.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos Aiko: " + devTenshi.getConteudosInscritos());
+        devAiko.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos inscritos Aiko: " + devTenshi.getConteudosInscritos());
+        System.out.println("Conteudos concluidos Aiko: " + devAiko.getConteudosConcluidos());
+        System.out.println("XP: " + devAiko.calcularTotalXp());
+
+
     }
 }
